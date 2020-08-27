feb7a4aa4af7f9e94d83b6b8d8a4c83758473832
jane muthoni <muchirijane@outlook.com>
2020-08-27 18:05:39 +0300

    adding async function

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const fs = require('fs');
 const path = require('path');
 const readmeBox = require('readme-box').ReadmeBox;
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const githubToken = core.getInput('github-token');
-    const filepath = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path')));
-    const data = fs.readFileSync(filepath, 'utf8');
-    console.log('GITHUB REF', process.env.GITHUB_REF.split('/')[2]);
+(async () => {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const githubToken = core.getInput('github-token');
+        const filepath = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path')));
+        const data = fs.readFileSync(filepath, 'utf8');
+        console.log('GITHUB REF', process.env.GITHUB_REF.split('/')[2]);
 
-    await readmeBox.updateSection(table, {
-        owner: process.env.GITHUB_REPOSITORY.split('/')[0],
-        repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-        branch: process.env.GITHUB_REF.split('/')[2],
-        token: githubToken,
-        section: 'data-section',
-    });
+        await readmeBox.updateSection(table, {
+            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
+            branch: process.env.GITHUB_REF.split('/')[2],
+            token: githubToken,
+            section: 'data-section',
+        });
 
-} catch (error) {
-    core.setFailed(JSON.stringify(error));
-}
+    } catch (error) {
+        core.setFailed(JSON.stringify(error));
+    }
+})();
